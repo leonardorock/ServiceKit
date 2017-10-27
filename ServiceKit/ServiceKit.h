fb490ee7baba31e07f4169c0470298cc40f37f99
Leonardo <leonardo_rock@icloud.com>
2017-10-27 10:03:57 -0200

    Initial Commit

---
+++ ServiceKit/ServiceKit.h
@@ -0,0 +1,19 @@
+//
+//  ServiceKit.h
+//  ServiceKit
+//
+//  Created by Leonardo Oliveira on 10/27/17.
+//  Copyright © 2017 KeyCar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ServiceKit.
+FOUNDATION_EXPORT double ServiceKitVersionNumber;
+
+//! Project version string for ServiceKit.
+FOUNDATION_EXPORT const unsigned char ServiceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ServiceKit/PublicHeader.h>
+
+
